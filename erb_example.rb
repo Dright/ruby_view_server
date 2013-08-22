465ec18937586abc62f4da3c412cee6c1e1a449c
Dright <as58140399@gmail.com>
2013-08-22 16:16:52 +0800

    add new feature to erb_example.rb

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,12 @@
 require 'erb'
 
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
-puts template.result(binding)
+str = "Hello!"
+array = ['first', 'second', 'third']
+hash = {:name => 'Dright', :age => 22}
+template1 = ERB.new "The value of x is: <%= x %>"
+template2 = ERB.new "array to upcase: <%= array.map { |e| e.upcase} %>"
+template3 = ERB.new "name in the hash: <%= hash[:name] %>"
+puts template1.result(binding)
+puts template2.result(binding)
+puts template3.result(binding)
